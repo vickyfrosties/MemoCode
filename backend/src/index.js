a0d0e618d86d4e614c152d094802c569ebdc9451
vickyfrosties <sounia.mhalla@hotmail.com>
2025-07-10 17:28:26 +0200

    feat(server): add start server function that connects to MongoDB database and starts server if connection with database is successfull

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import cors from "cors";
-import "dotenv/config";
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
 const app = express();
 const PORT = 8000;
-const { ATLAS_URI } = process.env.ATLAS_URI;
+dotenv.config();
 
 let corsOptions = {
   origin: "http://localhost:5173",
@@ -15,8 +16,27 @@ app.get("/notes", cors(corsOptions), (request, response) => {
   response.sendStatus(200);
 });
 
-// TODO : Créer une fonction pour lancer le serveur une fois connecté à la base de données.
+//* Fonction asynchrone qui initialise la connexion à la DB MongoDB et démarre le serveur Express si la connexion est réussie.
+async function startServer() {
+  //* Crée une instance du client MongoDB en utilisant l'URI stockée dans la variable d'environnement
+  const client = new MongoClient(process.env.ATLAS_URI);
+  try {
+    //* Le serveur établit la connexion avec la base de données via l'instance client
+    await client.connect();
+    console.log("MongoDB connected!");
+
+    //* Démarre le serveur Express sur le port défini
+    app.listen(PORT, () => {
+      console.log(`Server is running on PORT ${PORT}`);
+    });
+  } catch (error) {
+    //* Capture les erreurs liées à la connexion MongoDB et les affiche dans la console
+    console.error("Erreur de connexion DB :", error);
+  }
+}
 
 app.listen(PORT, () => {
   console.log("Server is running on:", { PORT });
 });
+
+startServer();
