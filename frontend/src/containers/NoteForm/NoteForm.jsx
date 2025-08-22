77600b45ca75299a6e9746318580505e9cf363f0
vickyfrosties <sounia.mhalla@hotmail.com>
2025-08-22 12:50:36 +0200

    style: add style responsive adjustements for form layout

--- frontend/src/containers/NoteForm/NoteForm.jsx
+++ frontend/src/containers/NoteForm/NoteForm.jsx
@@ -77,99 +77,116 @@ const NoteForm = ({ data, setFormData, resetDataForm }) => {
   return (
     <>
       <BackTo resetDataForm={resetDataForm} />
+      <section className={style["form-container"]}>
+        <form className={style["form"]} onSubmit={handleSubmit} id="note-form">
+          <div className={style["form_title_container"]}>
+            <label className={style["form_title_input"]}>
+              Title:
+              <input
+                type="text"
+                name="title"
+                id="title"
+                value={data.title}
+                autoFocus
+                autoComplete="off"
+                onChange={(e) =>
+                  setFormData({ ...data, title: e.target.value })
+                }
+              />
+              {formErrors.title && (
+                <p className={style["form_errors"]}>{formErrors.title}</p>
+              )}
+            </label>
 
-      <form className={style["form"]} onSubmit={handleSubmit} id="note-form">
-        <label className={style["form_title_input"]}>
-          Title:
-          <input
-            type="text"
-            name="title"
-            id="title"
-            value={data.title}
-            autoFocus
-            autoComplete="off"
-            onChange={(e) => setFormData({ ...data, title: e.target.value })}
-          />
-          {formErrors.title && (
-            <p className={style["form_errors"]}>{formErrors.title}</p>
-          )}
-        </label>
+            <label className={style["form_category_select"]}>
+              Category:
+              <select
+                name="category"
+                id="category-select"
+                onChange={(e) =>
+                  setFormData({ ...data, category: e.target.value })
+                }
+              >
+                <option value="">Choose a category</option>
+                <option value="programmation">Programmation</option>
+                <option value="neuropsychology">Neuropsychology</option>
+                <option value="astronomy">Astronomy</option>
+                <option value="gaming">Gaming</option>
+              </select>
+              {formErrors.category && (
+                <p className={style["form_errors"]}>{formErrors.category} </p>
+              )}
+            </label>
 
-        <label className={style["form_category_select"]}>
-          Category:
-          <select
-            name="category"
-            id="category-select"
-            onChange={(e) => setFormData({ ...data, category: e.target.value })}
-          >
-            <option value="">Choose a category</option>
-            <option value="programmation">Programmation</option>
-            <option value="neuropsychology">Neuropsychology</option>
-            <option value="astronomy">Astronomy</option>
-            <option value="gaming">Gaming</option>
-          </select>
-          {formErrors.category && (
-            <p className={style["form_errors"]}>{formErrors.category} </p>
-          )}
-        </label>
+            <label className={style["form_description_input"]}>
+              Description:
+              <textarea
+                name="description"
+                id="description"
+                value={data.description}
+                autoComplete="off"
+                rows={10}
+                cols={50}
+                onChange={(e) =>
+                  setFormData({ ...data, description: e.target.value })
+                }
+              />
+              <p className={style["form_validation_indication"]}>
+                characters up to: 2000 max
+              </p>
+              {formErrors.description && (
+                <p className={style["form_errors"]}>{formErrors.description}</p>
+              )}
+            </label>
+          </div>
 
-        <label className={style["form_description_input"]}>
-          Description:
-          <textarea
-            name="description"
-            id="description"
-            value={data.description}
-            autoComplete="off"
-            rows={10}
-            cols={50}
-            onChange={(e) =>
-              setFormData({ ...data, description: e.target.value })
-            }
-          />
-          <p className={style["form_validation_indication"]}>
-            characters up to: 2000 max
-          </p>
-          {formErrors.description && (
-            <p className={style["form_errors"]}>{formErrors.description}</p>
-          )}
-        </label>
+          <div className={style["form_attachment_input_container"]}>
+            <div className={style["form_attachment_input_container_block"]}>
+              <label className={style["form_attachment_input"]}>
+                Attachment:
+                <input
+                  type="url"
+                  name="picture"
+                  id="picture"
+                  value={data.picture}
+                  autoComplete="off"
+                  onChange={(e) =>
+                    setFormData({ ...data, picture: e.target.value })
+                  }
+                />
+                {formErrors.picture && (
+                  <p className={style["form_errors"]}>{formErrors.picture}</p>
+                )}
+              </label>
 
-        <label className={style["form_attachment_input"]}>
-          Attachment:
-          <input
-            type="url"
-            name="picture"
-            id="picture"
-            value={data.picture}
-            autoComplete="off"
-            onChange={(e) => setFormData({ ...data, picture: e.target.value })}
-          />
-          {formErrors.picture && (
-            <p className={style["form_errors"]}>{formErrors.picture}</p>
-          )}
-        </label>
+              <label className={style["form_link_input"]}>
+                Useful link:
+                <input
+                  type="url"
+                  name="link"
+                  id="link"
+                  value={data.link}
+                  autoComplete="off"
+                  onChange={(e) =>
+                    setFormData({ ...data, link: e.target.value })
+                  }
+                />
+                {formErrors.link && (
+                  <p className={style["form_errors"]}>{formErrors.link}</p>
+                )}
+              </label>
+            </div>
 
-        <label className={style["form_link_input"]}>
-          Useful link:
-          <input
-            type="url"
-            name="link"
-            id="link"
-            value={data.link}
-            autoComplete="off"
-            onChange={(e) => setFormData({ ...data, link: e.target.value })}
-          />
-          {formErrors.link && (
-            <p className={style["form_errors"]}>{formErrors.link}</p>
-          )}
-        </label>
-
-        <input
-          className={style["form_submit_button"]}
-          type="submit"
-          value={isSubmit ? "Submitting..." : "Submit"}
-        />
-      </form>
+            <div className={style["form_submit_button_container"]}>
+              <input
+                className={style["form_submit_button"]}
+                type="submit"
+                value={isSubmit ? "Submitting..." : "Submit"}
+              />
+            </div>
+          </div>
+        </form>
+      </section>
     </>
   );
 };
